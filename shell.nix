bc6015ddcf080fb3e039592f6d321fa036cd3289
toshinari123 <toshinaritong@gmail.com>
2024-04-06 19:38:47 +0800

    Use node 21

--- shell.nix
+++ shell.nix
@@ -20,7 +20,7 @@ pkgs.mkShell {
   passthru.env = env;
   buildInputs = [
     (env.nodeModules.override { prefix = "/lib/node_modules"; })
-    pkgs.nodejs
+    pkgs.nodejs_21
     js2nix.bin
     js2nix.proxy
     js2nix.node-gyp
